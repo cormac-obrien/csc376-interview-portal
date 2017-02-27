f15262a413f9199a521ff22884628104ed27b7a2
lalipio <lenny89@sbcglobal.net>
2017-02-26 21:06:43 -0600

    basic interview setup

--- serverthread.py
+++ serverthread.py
@@ -49,3 +49,128 @@ class ServerThread(threading.Thread):
 
     
         self.terminate_session()
+     
+      
+    # =========================================================================
+    #             INTERVIEW CREATION   
+    # Status: Incomplete
+    # 
+    # Precondition: 
+    # - Lawyer/Staff access
+    # - create interview option selected
+    #
+    # Postcondition:
+    # - question details recorded in database
+    #
+    # TO DO
+    # - encrypt/decrypt messages
+    # - add database interactions
+    # - finalize interview structure (e.g. multiple answers or single answers?)
+    # =========================================================================
+    def create_interview(self):
+        
+        # interview creation intro
+        self.client_socket.send('Interview Creation')
+        
+        # interview name entry
+        self.client_socket.send('Enter a name for the interview')
+        name = self.client_socket.recv(1024)
+        
+        # < name recorded here >
+        
+        # QUESTION CREATION LOOP
+        while(True):
+            
+            # question entry
+            self.client_socket.send('Enter a question')
+            print('You entered:')
+            print(self.client_socket.recv(1024))
+            
+            # ANSWER CREATION LOOP
+            while(True):
+                
+                #answer entry
+                self.client_socket.send('Enter the answer to the question')
+                print(self.client_socket.recv(1024))
+                
+                # < answer recorded here >
+                
+                
+                # < multiple answers? >
+                
+                break
+                
+            # SUBSEQUENT QUESTIONS LOOP
+            self.client_socket.send('Would you like to add another question?')
+            response = self.client_socket.recv(1024)
+            
+            # N - interview complete
+            if response == 'N':
+                
+                # < interview finalized here >
+                
+                break
+            # Y continue adding questions (repeat loop)
+            elif response == 'Y':
+                continue
+            # invalid response
+            else:
+                self.client_socket.send('Invalid Response!')
+        pass
+    
+    # ===========================================================================
+    #             INTERVIEW ASSIGNMENT   
+    # Status: Incomplete
+    # 
+    # Precondition(s): 
+    # - completed interview
+    # - interviewee exists
+    #
+    # Postcondition:
+    # - interviewee can access interview
+    #
+    # TO DO
+    # - encrypt/decrypt messages
+    # - add database interactions
+    # - finalize interview assignment design (e.g. single or multiple assignment?)
+    # =============================================================================
+    def assign_interview(self):
+        
+        # < followup on interview creation or unique menu? >
+        
+        pass
+    
+    # ===========================================================================
+    #             INTERVIEW Review   
+    # Status: Incomplete
+    # 
+    # Precondition(s):
+    # - assigned interview completed by interviewee
+    # - interview recorded in database
+    #
+    # Postcondition: 
+    # - can be viewed by Lawyer/Staff
+    #
+    # TO DO
+    # - encrypt/decrypt messages
+    # - add database interactions
+    # - finalize interview review design 
+    # =============================================================================
+    def review_interview(self):
+
+        # interview creation intro
+        self.client_socket.send('Interview Inbox')
+        
+        # interview retrieval
+        # < search criteria? name, id? >
+        self.client_socket.send('')
+        self.client_socket.recv(1024)
+        
+        # < retrieve interview from database >
+        # < send ID details >
+        self.client_socket.send()
+        
+        # < INTERVIEW QUESTIONS LOOP >
+        
+        pass
+        
